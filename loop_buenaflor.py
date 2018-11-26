731c5c53ca8a5f7f78638237ba0fc84bd060f4cd
reivinbuenaflor <45374752+reivinbuenaflor@users.noreply.github.com>
2018-11-27 07:49:10 +0800

    Loop

---
+++ loop_buenaflor.py
@@ -0,0 +1,16 @@
+#loop by 2
+print("By 2")
+for x in range(2,11,2):
+    print(x)
+#loop by 3
+print("By 3")
+for x in range(3,13,3):
+    print(x)
+#loop by 4
+print("By 4")
+for x in range(4,17,4):
+    print(x)
+#loop by 12
+print("By 12")
+
+
