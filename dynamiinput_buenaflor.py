e994cae5ef44bcd918294f66fdf02a669187ec15
reivinbuenaflor <45374752+reivinbuenaflor@users.noreply.github.com>
2018-11-27 07:47:39 +0800

    Dynamic Input

---
+++ dynamiinput_buenaflor.py
@@ -0,0 +1,11 @@
+#Accepts input from user
+name = input("Enter your name: ")
+print("Your name is: " + name)
+
+#Accepts numerical input
+fNum = int(input("Enter a first value: "))
+sNum = int(input("Enter a second value: "))
+intVal = int(input("Enter a value for integer: "))
+
+print(fNum + sNum)
+print(intVal ** 2)
