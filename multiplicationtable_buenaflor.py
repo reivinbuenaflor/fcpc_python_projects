49a63ca78d35f78744007deb5d58cc08f5b47f54
reivinbuenaflor <45374752+reivinbuenaflor@users.noreply.github.com>
2018-11-27 07:50:21 +0800

    Multiplication Table

---
+++ multiplicationtable_buenaflor.py
@@ -0,0 +1,6 @@
+#Nested Loop Multiplication Table 10 - 20
+print("Nested Loop Multiplication Table 10 - 20")
+for z in range(10,21):
+    for y in range(1,11):
+        print(z*y, end='\t')
+    print()
