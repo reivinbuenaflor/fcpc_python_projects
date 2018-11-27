80c70a47083d90ffba93c28c5c1624f1decc81fe
reivinbuenaflor <buenaflorreivin@yahoo.com>
2018-11-27 10:40:59 +0800

    Activity A

---
+++ activity_a_buenaflor.py
@@ -0,0 +1,9 @@
+for x in range(1, 17 , 3):
+    print(x, end = '\t')
+for y in range(100, 78, -7):
+    print(y, end = '\t')
+for z in range(1 , 5):
+    if z % 3 == 0:
+        print(1, end = '\t')
+    else:
+        print(0, end = '\t')
